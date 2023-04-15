c000b6b65fedca32f791989543cfa0a02c778047
VimalChandran777 <vimalchandran.r2021ecec@sece.ac.in>
2023-04-15 12:36:53 +0100

    updated

--- data.js
+++ data.js
@@ -1,3 +1,28 @@
 const a = location.href.split("?")[1]
 const img = document.getElementById("img")
 img.setAttribute("src",`../img/${a}.png`)
+
+const staff = 
+    {
+        'dumbel': '#FFECE8',
+        'hagridd':'#FFECE8',
+        'lup':'#FFECE8',
+        'mc':'#FFECE8',
+        'umbr':'#D9F7F7',
+        'snap':'#D9F7F7'
+    }
+const student =
+    {
+        'cho':'#DEFAFA',
+        'dracoo':'#D9F7F7',
+        'harry':'#FFECE8',
+        'ron':'#FFECE8',
+        'cegridd':'#FFFFDF',
+        'hermo':'#FFECE8'
+    }
+const isStaff = staff[a]
+if (isStaff) {
+    document.body.style.backgroundColor = staff[a]
+} else {
+    document.body.style.backgroundColor = student[a]
+}
--- jason.js
+++ jason.js
@@ -1,1 +1,11 @@
-  
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (username.value === localStorage.getItem('username') && password.value === localStorage.getItem('password')) {
+        location.href =  "/Staff.html"
+    } else {
+        alert('Invalid Credentials')
+    }
+})
